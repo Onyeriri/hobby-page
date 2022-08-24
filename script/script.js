7010130202e7b7b4e250abe2860f028bcf0d4d47
Onyeriri Victor <viktorchinonso@gmail.com>
2022-08-24 21:39:40 +0100

    Added comments to script file

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// date object
 let dateOption = {
   weekday: 'long',
   year: 'numeric',
@@ -5,8 +6,10 @@ let dateOption = {
   day: 'numeric'
 }
 
+// initialize date variable
 let date = new Date();
 
+// initialize HTML element
 const element = document.getElementById('date');
 
 element.innerHTML = date.toLocaleDateString('en-GB', dateOption) + '.';
